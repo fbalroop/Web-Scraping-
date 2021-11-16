139cf6f6c198725d1115bdb51cf875c9251ebd57
fbalroop <89998799+fbalroop@users.noreply.github.com>
2021-11-15 20:08:51 -0500

    Update webscraping.R

--- webscraping.R
+++ webscraping.R
@@ -13,9 +13,9 @@ html %>%
   head(n=1) # take the first 1st records
 
 ##code to webscrape a list of synonyms for the merriam webster thesaurus 
-words <- c("department","surgery","staff","Doctor","consent")
+words <- c("research","department","staff","Doctor","consent")
 htmls <- paste0("https://www.merriam-webster.com/thesaurus/", words)
-getwd()
+##getwd()
 
 info_list <-  map(htmls, ~ .x   %>%
                    read_html() %>%
@@ -24,14 +24,8 @@ info_list <-  map(htmls, ~ .x   %>%
 
 df <- data.frame(matrix(unlist(info_list), nrow=length(info_list), byrow=T))
 
-words<-data.frame(words,df)
-
-words %>% write_csv("webscrapetest.csv")
+df$synonymns <- gsub('\\s+', '', df$matrix.unlist.info_list...nrow...length.info_list...byrow...T.)
 
 
-POSpeech <-  map(htmls, ~ .x   %>%
-                    read_html() %>%
-                    html_node(".important-blue-link") %>%
-                    html_text())
-
-df2pos <- matrix(unlist(POSpeech), nrow=length(POSpeech), byrow=T)
+words<-data.frame(words,df) %>% 
+  select(words,synonymns)
